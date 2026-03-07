77784c82dd27aab7177bbc0a22d20305d187c3e8
luisDaniel <estudiosluisperez@gmail.com>
2026-03-06 22:11:58 -0500

    fix : se tenia un error logico de guarado de datos , tenia mal instanciada las clases

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from registro.ingreso import RegistrarIngreso
 from registro.salida import RegistroSalida
 
-def registrarIngresoVehiculoMain():
-    # se instancia la clase / al parecer es obligatorio
-    registrarIngreso = RegistrarIngreso()
+def registrarIngresoVehiculoMain(ingreso):
     
     # recibimos los datos
     placa = input("ingrese la placa: ")
@@ -11,11 +9,10 @@ def registrarIngresoVehiculoMain():
     horaIngreso = input("ingrese la hora: ")
     
     # se envian a registrar los datos enviados
-    registrarIngreso.reguistrarVehiculo(placa , tipo , horaIngreso)
+    ingreso.reguistrarVehiculo(placa , tipo , horaIngreso)
         
-def registrarSalidaVehiculoMain():
-    # se instancia la clase 
-    salida = RegistroSalida()
+def registrarSalidaVehiculoMain(salida):
+    
     
     # se obtiene la placa
     placa = input("ingrese la placa: ")
@@ -25,19 +22,23 @@ def registrarSalidaVehiculoMain():
     
 
 if __name__ == "__main__":
+    # se instancia la clase 
+    ingreso = RegistrarIngreso() 
+    salida = RegistroSalida()
     
     for  v in range(5):
-        print(f"""
-              - {v} -
-              - menu -
-              1 para ingresar 
-              2 para salir
-              """)
+        print(
+f"""
+  - {v} -
+  - menu -
+  1 para ingresar 
+  2 para salir
+""")
         opcion = int(input("opcion aqui: "))
         if opcion == 1:
-            registrarIngresoVehiculoMain()
+            registrarIngresoVehiculoMain(ingreso)
         elif opcion == 2:
-            registrarSalidaVehiculoMain()
+            registrarSalidaVehiculoMain(salida)
     
     
  
