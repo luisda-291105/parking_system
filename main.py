6d20e9781107a05481046d5d53615bfa2ffb8b51
luisDaniel <estudiosluisperez@gmail.com>
2026-03-06 20:46:01 -0500

    feat :  se crearon funciones de registro de inicio y registro de salida en el modulo principal  y  se agrego una validacion sencilla tipo menu de opciones

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
 from registro.ingreso import RegistrarIngreso
+from registro.salida import RegistroSalida
 
-
+def registrarIngresoVehiculoMain():
+    # se instancia la clase / al parecer es obligatorio
+    registrarIngreso = RegistrarIngreso()
+    
+    # recibimos los datos
+    placa = input("ingrese la placa: ")
+    tipo = input("ingrese el tipo: ")
+    horaIngreso = input("ingrese la hora: ")
+    
+    # se envian a registrar los datos enviados
+    registrarIngreso.reguistrarVehiculo(placa , tipo , horaIngreso)
+        
+def registrarSalidaVehiculoMain():
+    # se instancia la clase 
+    salida = RegistroSalida()
+    
+    # se obtiene la placa
+    placa = input("ingrese la placa: ")
+    
+    salida.buscarVehiculo(placa)
+    
+    
 
 if __name__ == "__main__":
     
-    registro_ingreso = RegistrarIngreso()
+    opcion = int(input("ingrese // 1 para ingresar , 2 para salir: "))
+    if opcion == 1:
+        registrarIngresoVehiculoMain()
+    elif opcion == 2:
+        registrarSalidaVehiculoMain()
     
-
-    for i in range(5) : 
-        placa = input("ingrese la placa: ")
-        tipo = input("ingrese el tipo: ")
-        horaIngreso = input("ingrese la hora: ")
-
-        registro_ingreso.reguistrarVehiculo(placa , tipo , horaIngreso ) 
+    
+ 
      
 
 
