dae6d273b25f3a31705ed86b3902fca1fd3d07fd
luisDaniel <estudiosluisperez@gmail.com>
2026-03-11 16:40:09 -0500

    feat: se creo la nueva funcionalidad de calcualar la tarifa a pagar

--- registro/salida.py
+++ registro/salida.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 
 class RegistroSalida():
     
@@ -13,26 +14,39 @@ class RegistroSalida():
             print("⚠️ No hay vehículos parqueados")
             return None
         
-        if self.ingreso.vehiculosParquiados:
-            print("vehiculos parquiados actualmente :")
-            print(f"{self.ingreso.vehiculosParquiados}")
-
-        
        # Buscar el vehículo
         for i, vehiculo in enumerate(self.ingreso.vehiculosParquiados):
             if vehiculo["placa"] == placa:
+                salida = input("ingrese la hora salida (HH:MM)")
                 print(f"✅ Vehículo encontrado:")
-                print(f"   Placa: {vehiculo['placa']}")
                 print(f"   Tipo: {vehiculo['tipo']}")
+                print(f"   Placa: {vehiculo['placa']}")
                 print(f"   Ingreso: {vehiculo['horaIngreso']}")
+                print(f"   Salida: {salida}")
+                self.calcularTarifa(salida , vehiculo)
                 return vehiculo  # Retornar el vehículo encontrado
             
         # Si no se encontró
         print(f"❌ No se encontró vehículo con placa {placa}")
         return None
     
-    def calcularTarifa(self):
-        pass
-
+    def calcularTarifa(self , salidaInput ,  vehiculo  ):
+        # agregamos la hora de salida al objeto vehiculo seleccionado
+        vehiculo['horaSalida'] = salidaInput
+        # declaran datos fijos y el formato de la hora
+        pago_Hora = 1500.0
+        formato = "%H:%M"
+        # proceso para pasar la hora recibida en formato del tiempo (%H:%M)
+        hora_entrada = datetime.strptime(vehiculo["horaIngreso"] , formato)
+        hora_salida = datetime.strptime(vehiculo['horaSalida']  , formato)
+        # proceso para calcular las horas trabajadas y el total a pagar
+        hora_trabajadas = (hora_salida - hora_entrada).seconds / 3600
+        total_pagar = hora_trabajadas * pago_Hora
+        
+        # muestra la tarifa
+        print("tarifa gago")
+        print(f"   horas trabajadas: {hora_trabajadas}")
+        print(f"   total a pagar: {total_pagar}")
+        
     def registrarPago(self):
         pass
