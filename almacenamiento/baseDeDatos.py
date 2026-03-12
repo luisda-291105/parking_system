14d3c525f5032942f6609d61af8e560474a25b3d
luis daniel perez pinedo <luisdaperezpinedo@gmail.com>
2026-03-12 01:48:00 +0000

    style : se organizaron los comentarios

--- almacenamiento/baseDeDatos.py
+++ almacenamiento/baseDeDatos.py
@@ -1,13 +1,10 @@
-# 1 se importa el sismeta a usar
-import sqlite3
 
-# 2 se crea la coneccion o se une a la ingresada
-conexion = sqlite3.connect("dataBase")
-# 3 el cursor es como los brasos , opera el crud 
-cursorBD = conexion.cursor()
+import sqlite3 # 1 se importa el sismeta a usar
 
-# 4 se verifica si la tabla existe o se crea una nueva 
-def tablaExiste(nombreTabla):
+conexion = sqlite3.connect("dataBase") # 2 se crea la coneccion o se une a la ingresada
+cursorBD = conexion.cursor() # 3 el cursor es como los brasos , opera el crud
+
+def tablaExiste(nombreTabla): # 4 se verifica si la tabla existe o se crea una nueva 
     # 5 con esta consulta busco en la tabla maestra de sqlite la tabla a buscar
     cursorBD.execute(''' SELECT COUNT(name) FROM SQLITE_MASTER WHERE TYPE = 'table' AND name = '{}' '''.format(nombreTabla))
     
@@ -23,24 +20,13 @@ def tablaExiste(nombreTabla):
         
         return False
 
-
-# 7  con esta funcion busco hacer la funcionalidad del crud : crear 0 insertar datos 
-def ingresarDatos(placa , tipo , hora_entrada , hora_salida , total_pagar):
+def ingresarDatos(placa , tipo , hora_entrada , hora_salida , total_pagar): # 7  con esta funcion busco hacer la funcionalidad del crud : crear 0 insertar datos 
     # 8 consulta que se usa para isertar los datos 
     cursorBD.execute('''INSERT INTO VEHICULOS (PLACA , TIPO , HORA_ENTRADA , HORA_SALIDA , TOTAL_PAGAR) VALUES (?,?,?,?,?)''',(placa, tipo, hora_entrada, hora_salida, total_pagar) )
     # con esta instrucccion se hacen oficiales los cambios creados 
     conexion.commit()
-    print(f" datos insertados correctamente en VEHICULOS")
-    print(f" placa : {placa}")
-    print(f" tipo : {tipo}")
-    print(f" hora_entrada : {hora_entrada}")
-    print(f" hora_salida : {hora_salida}")
-    print(f" total_pagar : {total_pagar}")
-                
-
-
-# 9 funcion para buscar todos los datos de un vehiculo
-def buscarDatosID(id):
+         
+def buscarDatosID(id): # 9 funcion para buscar todos los datos de un vehiculo
     # 10 consulta que busca la fila que coinsida con el id
     cursorBD.execute(''' SELECT * FROM VEHICULOS WHERE ID = '{}' '''.format(id))
 
@@ -54,7 +40,6 @@ def buscarDatosID(id):
     else:
         print(f" vehiculos : {vehiculos}")
         return True
-
 
 
 # ingresarDatos('QXB87F' , 'MOTO' ,'1:00' , '5:00', '8000' )
