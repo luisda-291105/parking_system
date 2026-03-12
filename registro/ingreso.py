d1b9f2b276d17bc2bdbd674d29f19c07b9b92a7a
luisDaniel <estudiosluisperez@gmail.com>
2026-03-12 01:42:20 -0500

    feat : se implemento el manejo de la base de datos , busca en la base de datos , muestra , guarda cuando se registra un ingreso ,  en la salida falta trabajar

--- registro/ingreso.py
+++ registro/ingreso.py
@@ -1,12 +1,17 @@
+from almacenamiento.baseDeDatos import ingresarDatos , buscarTodos
+
+
 class RegistrarIngreso:
     
     def __init__(self):
+        self.data = []
         self.espacios = 5
-        self.vehiculosParquiados = []
-        
-        
+        self.vehiculosParquiados = self.data + buscarTodos()
+
     # funcion que valida si hay o no espacio para parquiar
     def validarEspacio(self):
+        var = self.vehiculosParquiados + self.data
+
         if len(self.vehiculosParquiados) >= self.espacios:
             print("❌ No hay espacios disponibles")
             return False
@@ -15,22 +20,24 @@ class RegistrarIngreso:
             return True
         
     # funcion que registra el vehiculo 
-    def reguistrarVehiculo(self , placa , tipo , horaIngreso , horaSalida="none"):
-        
+    def reguistrarVehiculo(self , placa , tipo , horaIngreso , horaSalida="none" , totalPagar = 'none'):
+
         if not self.validarEspacio():
             return False
         
-        tuplaVehiculo = (placa , tipo)
+        # tuplaVehiculo = (placa , tipo)
         
         vehiculo = {
-            "PLACA" : tuplaVehiculo[0] ,
-            "TIPO" : tuplaVehiculo[1] ,
+            "PLACA" : tipo ,
+            "TIPO" : placa ,
             "HORA_ENTRADA" : horaIngreso,
-            "HORA_SALIDA" : horaSalida
+            "HORA_SALIDA" : horaSalida,
+            "TOTAL_PAGAR" : totalPagar
         }
         
         self.vehiculosParquiados.append(vehiculo)
-  
+
+        ingresarDatos(vehiculo["PLACA"], vehiculo["TIPO"], vehiculo["HORA_ENTRADA"],vehiculo['HORA_SALIDA'] , vehiculo["TOTAL_PAGAR"])
         print("vehiculo registrado correctamente")
         print("\n")
 
@@ -44,10 +51,12 @@ class RegistrarIngreso:
        
     # funcion que retorna la lista de vehiculos parquiados 
     def mostrarTodosVehiculos(self):
+
         if not self.vehiculosParquiados:
             print("⚠️ No hay vehículos")
             return
         
         print(f"\n📋 VEHÍCULOS ({len(self.vehiculosParquiados)}/{self.espacios}):")
-        for i, v in enumerate(self.vehiculosParquiados, 1):
-            print(f"{i}. {v['PLACA']} - {v['TIPO']} - {v['HORA_ENTRADA']} - {v['HORA_SALIDA']}")
+        for v in self.vehiculosParquiados:
+            print(f"vehiculos parquiados {v}")
+            #print(f"{v['ID']}. {v['PLACA']} - {v['TIPO']} - {v['HORA_ENTRADA']} - {v['HORA_SALIDA']} - {v['TOTAL_PAGAR']}")
