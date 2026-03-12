fb179f37b64735558b0fbc48e3ea9cd28a096d54
luisDaniel <estudiosluisperez@gmail.com>
2026-03-12 01:40:29 -0500

    feat : se le aplicaron el manejo de excepciones en todas las funciones

--- almacenamiento/baseDeDatos.py
+++ almacenamiento/baseDeDatos.py
@@ -1,92 +1,128 @@
+import sqlite3
 
-import sqlite3 # 1 se importa el sismeta a usar
+# se crea la coneccion o se une a la ingresada
+# el cursor es como los brasos , opera el crud
+conexion = sqlite3.connect("dataBase")
+cursorBD = conexion.cursor()
 
-conexion = sqlite3.connect("dataBase") # 2 se crea la coneccion o se une a la ingresada
-cursorBD = conexion.cursor() # 3 el cursor es como los brasos , opera el crud
+# se verifica si la tabla existe o se crea una nueva
+def tablaExiste(nombreTabla):
+    # con esta consulta busco en la tabla maestra de sqlite la tabla a buscar
+    try:
+        cursorBD.execute(
+            ''' SELECT COUNT(name) FROM SQLITE_MASTER WHERE TYPE = 'table' AND name = '{}' '''.format(nombreTabla))
 
-def tablaExiste(nombreTabla): # 4 se verifica si la tabla existe o se crea una nueva 
-    # 5 con esta consulta busco en la tabla maestra de sqlite la tabla a buscar
-    cursorBD.execute(''' SELECT COUNT(name) FROM SQLITE_MASTER WHERE TYPE = 'table' AND name = '{}' '''.format(nombreTabla))
-    
-    # 6 con esta condicion busco validar la cantidad de tablas recividas desde la consulta 
-    if cursorBD.fetchone()[0] >= 1:
-        # en caso de exister solo se sale
-        print(f"la tabla {nombreTabla} ya existe")
-        return True
-    else:
-        # en caso de no existir la crea de nuevo
-        cursorBD.execute(''' CREATE TABLE VEHICULOS (ID INTEGER PRIMARY KEY AUTOINCREMENT, PLACA TEXT , TIPO TEXT , HORA_ENTRADA TEXT , HORA_SALIDA TEXT , TOTAL_PAGAR REAL ) ''')
-        print(f"la tabla {nombreTabla} fue creada exitozamente")
-        
-        return False
+        # 6 con esta condicion busco validar la cantidad de tablas recividas desde la consulta
+        if cursorBD.fetchone()[0] >= 1:
+            # en caso de exister solo se sale
+            print(f"la tabla {nombreTabla} ya existe")
+            return True
+        else:
+            # en caso de no existir la crea de nuevo
+            cursorBD.execute(
+                ''' CREATE TABLE {} (ID INTEGER PRIMARY KEY AUTOINCREMENT, PLACA TEXT , TIPO TEXT , HORA_ENTRADA TEXT , HORA_SALIDA TEXT , TOTAL_PAGAR REAL ) '''.format(
+                    nombreTabla))
+            print(f"la tabla {nombreTabla} fue creada exitosamente")
+
+            return False
+    except sqlite3.Error as error:
+        print(f'error en la funcion tabla existe! {error}')
 
 tablaExiste('VEHICULOS')
 
-def ingresarDatos(placa , tipo , hora_entrada , hora_salida , total_pagar): # 7  con esta funcion busco hacer la funcionalidad del crud : crear 0 insertar datos 
-    # 8 consulta que se usa para isertar los datos 
-    cursorBD.execute('''INSERT INTO VEHICULOS (PLACA , TIPO , HORA_ENTRADA , HORA_SALIDA , TOTAL_PAGAR) VALUES (?,?,?,?,?)''',(placa, tipo, hora_entrada, hora_salida, total_pagar) )
-    # con esta instrucccion se hacen oficiales los cambios creados 
-    conexion.commit()
-         
-def buscarDatosID(id): # 9 funcion para buscar todos los datos de un vehiculo
-    # 10 consulta que busca la fila que coinsida con el id
-    cursorBD.execute(''' SELECT * FROM VEHICULOS WHERE ID = '{}' '''.format(id))
+#  con esta funcion busco hacer la funcionalidad del crud : crear 0 insertar datos
+def ingresarDatos(placa, tipo, hora_entrada, hora_salida, total_pagar):
+    try:
+        # consulta que se usa para isertar los datos
+        cursorBD.execute('''INSERT INTO VEHICULOS (PLACA, TIPO, HORA_ENTRADA, HORA_SALIDA, TOTAL_PAGAR) VALUES (?, ?, ?, ?, ?)''', (placa, tipo, hora_entrada, hora_salida, total_pagar))
+        # con esta instrucccion se hacen oficiales los cambios creados
+        conexion.commit()
 
-    # 11 obtengo los datos de la consulta
-    vehiculo = cursorBD.fetchall()
+    except sqlite3.Error as error:
+        print(f'error en la funcion insertarDatos! {error}')
 
-    # 12 valido si tiene o no contenido
-    if len(vehiculo) == 0 :
-        print(f" vehiculos : no encontrado")
-        return False
-    else:
+#  funcion para buscar todos los datos de un vehiculo
+def buscarDatosID(id):
+    try:
+        #  consulta que busca la fila que coinsida con el id
+        cursorBD.execute(''' SELECT * FROM VEHICULOS WHERE ID = '{}' '''.format(id))
 
-        return  vehiculo
+        #  obtengo los datos de la consulta
+        vehiculo = cursorBD.fetchall()
 
-def buscarPlaca(placa):
-    cursorBD.execute(''' SELECT * FROM VEHICULOS WHERE PLACA = '{}' '''.format(placa))
-    vehiculo = cursorBD.fetchall()
-
-    if len(vehiculo) == 0:
-        print(f" vehiculos : no encontrado")
-        return False
-    else:
-        print(f"veiculo encontrado {vehiculo}")
-        return True
-
-def buscarTodos():
-    cursorBD.execute(''' SELECT * FROM VEHICULOS ''')
-    vehiculos = cursorBD.fetchall()
-    if len(vehiculos) == 0 :
-        print(f" vehiculos : no encontrado")
-    else:
-        print(F"vehiculos parquiados ")
-        for vehiculo in vehiculos:
-            print(f"vehiculo  = {vehiculo}")
-
-    return vehiculos
-
-def actualizarID(id , diccionario):
-    valoresValidos = ['PLACA' , 'TIPO' , 'HORA_ENTRADA' , 'HORA_SALIDA' , 'TOTAL_PAGAR' ]
-    for key in diccionario.keys():
-        if key not in valoresValidos:
-            print("clave incorrecta")
-            raise Exception("clave incorrecta")
+        #  valido si tiene o no contenido
+        if len(vehiculo) == 0:
+            print(f" vehiculos : no encontrado")
+            return False
         else:
-            cursorBD.execute(''' UPDATE VEHICULOS SET {} = '{}' WHERE ID = {} '''.format(key, diccionario[key], id))
 
-    conexion.commit()
+            return vehiculo
+    except sqlite3.Error as error:
+        print(f'error en la funcion buscarDatosID! {error}')
 
-def eliminarID(id): # funcion parea eliminar filtrando por id
-    # 15 esta parte es para mostrar los datos que se eliminaron por consola
-    print(f" datos eliminado: {buscarDatosID(id)}")
+# funcion para filtrar vehiculo por la placa
+def buscarPlaca(placa):
+    try:
+        cursorBD.execute(''' SELECT * FROM VEHICULOS WHERE PLACA = '{}' '''.format(placa))
+        vehiculo = cursorBD.fetchall()
 
-    # 13 consulta para eliminar por id
-    cursorBD.execute(''' DELETE FROM VEHICULOS WHERE ID = '{}' '''.format(id))
-    # 14 se hacen oficiales los cambios
-    conexion.commit()
+        if len(vehiculo) == 0:
+            print(f" vehiculos : no encontrado")
+            return False
+        else:
+            print(f"veiculo encontrado {vehiculo}")
+            return True
+    except sqlite3.Error as error:
+        print(f'error en la funcion buscarPlaca! {error}')
 
+# busca todos los vehiculos guardados en la base de datos
+def buscarTodos():
+    try:
+        cursorBD.execute(''' SELECT * FROM VEHICULOS ''')
+        vehiculos = cursorBD.fetchall()
+        if len(vehiculos) == 0:
+            print(f" vehiculos : no encontrado")
+        else:
+            #print(F"vehiculos parquiados ")
+            #for vehiculo in vehiculos:
+            #    print(f"vehiculo  = {vehiculo}")
+
+            return vehiculos
+    except sqlite3.Error as error:
+        print(f'error en la funcion buscarTodos! {error}')
+
+# actualiza valores de los objetos en la base de datos ya creadas
+def actualizarID(id, diccionario):
+    try:
+        valoresValidos = ['PLACA', 'TIPO', 'HORA_ENTRADA', 'HORA_SALIDA', 'TOTAL_PAGAR']
+        for key in diccionario.keys():
+            if key not in valoresValidos:
+                print("clave incorrecta")
+                raise Exception("clave incorrecta")
+            else:
+                cursorBD.execute(''' UPDATE VEHICULOS SET {} = '{}' WHERE ID = {} '''.format(key, diccionario[key], id))
+
+        conexion.commit()
+    except sqlite3.Error as error:
+        print(f'error en la funcion actualizarID! {error}')
+
+# funcion parea eliminar filtrando por id
+def eliminarID(id):
+    try:
+        #  esta parte es para mostrar los datos que se eliminaron por consola
+        print(f" datos eliminado: {buscarDatosID(id)}")
+
+        # consulta para eliminar por id
+        cursorBD.execute(''' DELETE FROM VEHICULOS WHERE ID = '{}' '''.format(id))
+        # se hacen oficiales los cambios
+        conexion.commit()
+    except sqlite3.Error as error:
+        print(f'error en la funcion eliminarID! {error}')
+
+# funcion que elimina todos los datos de una tabla , solo en caso de emergencia
 def eliminarTodos():
-    cursorBD.execute(''' DELETE FROM VEHICULOS ''')
-    conexion.commit()
-
+    try:
+        cursorBD.execute(''' DELETE FROM VEHICULOS ''')
+        conexion.commit()
+    except sqlite3.Error as error:
+        print(f'error en la funcion  eliminarTodos! {error}')
