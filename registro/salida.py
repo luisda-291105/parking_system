91d17ebc9de0b281e1336beccd97752bccc5b85e
luisDaniel <estudiosluisperez@gmail.com>
2026-03-27 17:06:12 -0500

    feat : se creo una nueva funcion para imprimir los vehiculos encontrados
    feat :se agregan el total a pagar , las horas parquiadas al diccionario vehiculo
    
    feat : se agrego una nueva validacion en la funcino buscar vehuculo para que valide si ya salio el vehiculo o no

--- registro/salida.py
+++ registro/salida.py
@@ -21,6 +21,7 @@ class RegistroSalida():
     
     def __init__(self , ingreso):
         self.ingreso = ingreso
+    
     #  funcion que inicia el modulo
     def initExit(self):
         # se obtiene la placa
@@ -28,6 +29,7 @@ class RegistroSalida():
         
         # se busca la placa para regustrar la salida
         self.buscarVehiculo(placa)
+    
     # funcion que busca la existencia del vehiculo a salir
     def buscarVehiculo(self , placa ):
         
@@ -40,19 +42,21 @@ class RegistroSalida():
         
        # Buscar el vehículo
         for i, vehiculo in enumerate(self.ingreso.vehiculosParquiados):
+            
             if vehiculo["placa"] == placa:
+                if vehiculo["horaSalida"] != None:
+                    print(f"vehiculo de placa '{placa}' ya no se encuentra parquiado") 
+                    break
+                
                 salida = input("ingrese la hora salida (HH:MM)")
-                print(f"✅ Vehículo encontrado:")
-                print(f"   Tipo: {vehiculo['tipo']}")
-                print(f"   Placa: {vehiculo['placa']}")
-                print(f"   Ingreso: {vehiculo['horaIngreso']}")
-                print(f"   Salida: {salida}")
-                self.calcularTarifa(salida , vehiculo)
-                return vehiculo  # Retornar el vehículo encontrado
+                
+                self.calcularTarifa(salida , vehiculo) 
+                return vehiculo        
             
         # Si no se encontró
         print(f"❌ No se encontró vehículo con placa {placa}")
         return None
+    
     # funcion que calcula e imprime  el la factura 
     def calcularTarifa(self , salidaInput ,  vehiculo  ):
         # agregamos la hora de salida al objeto vehiculo seleccionado
@@ -67,8 +71,22 @@ class RegistroSalida():
         hora_trabajadas = (hora_salida - hora_entrada).seconds / 3600
         total_pagar = hora_trabajadas * pago_Hora
         
+        vehiculo["hora_trabajadas"] = hora_trabajadas
+        vehiculo["total_pagar"] = total_pagar
+        
+        self.imprimirVehiculoEncontrado( vehiculo)
+        
+    def imprimirVehiculoEncontrado(self , vehiculo):
+        print("\n")
+        print(f"✅ Vehículo encontrado:")
+        print(f"   Tipo: {vehiculo['tipo']}")
+        print(f"   Placa: {vehiculo['placa']}")
+        print(f"   Ingreso: {vehiculo['horaIngreso']}")
+        print(f"   Salida: {vehiculo['horaSalida']}")
+        
+        print("\n")
         # muestra la tarifa
-        print("tarifa gago")
-        print(f"   horas trabajadas: {hora_trabajadas}")
-        print(f"   total a pagar: {total_pagar}")
+        print("    TARIFA")
+        print(f"   horas parquiado: {vehiculo['hora_trabajadas']}")
+        print(f"   total a pagar: {vehiculo['total_pagar']}")
         
