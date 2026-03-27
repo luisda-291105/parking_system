e52806d37fb77228ed0a616578659e26526a48d5
luisDaniel <estudiosluisperez@gmail.com>
2026-03-27 18:42:02 -0500

    feat : se creo la funcion que lee y convierte la lista de diccionario guardada en el cvs a una lista de diccionario

--- almacenamiento/fileDB.py
+++ almacenamiento/fileDB.py
@@ -1,3 +1,5 @@
+import ast
+
 """ este modulo se va a encargar de  """
 # read() = leer el contenido del archivo parquiadero.txt
 # write() = crear si no existe y de agregar los nuevos vehiculos despues de la salida
@@ -5,12 +7,18 @@
 
 storage = "parquiadero.csv" 
 
-
-    
 def write(vehiculo):
     with open(storage , "a" , encoding="utf-8" ) as archivo:
             archivo.write( f"{vehiculo}  \n")
             
-        
+def read():
+    vehiculos = []
+    with open(storage, "r" , encoding="utf-8") as archivo:
+
+        for linea in archivo:
+            vehiculo = ast.literal_eval(linea.strip())
+            vehiculos.append(vehiculo)
+ 
+    return vehiculos      
 
         
